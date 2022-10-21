8b31407cf44724c8982e1df3e4eb60af0f41be19
Rafael Mauricio da SIlva <111100311+Rafael-M-Silva@users.noreply.github.com>
2022-10-21 11:24:31 -0300

    Sofisticando o código
    Tirei as linha de código JS que estava no HTML

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# Tabuada
-Tabuada desenvolvida com Js - Primeira interação com Js
+<h1 align="center"> Tabuada </h1>
+
+
+<p align="center">
+  <a href="#-tecnologias">Tecnologias</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-projeto">Projeto</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-layout">Layout</a>
+</p>
+
+<br>
+
+## 🚀 Tecnologias
+
+Esse projeto foi desenvolvido com as seguintes tecnologias:
+
+- HTML e CSS
+- JavaScript
+
+## 💻 Projeto
+
+Tabuada do 10, sempre atualizando e sofisticando o código.
+
--- js/script.js
+++ js/script.js
@@ -1,14 +1,15 @@
-function verificar(){
-    let tabuada = document.querySelector('#tab');
-    let res = document.querySelector('#res');
-    let tab = Number(tabuada.value);
-    if (tabuada.value.length == 0 || tab == 0){
-        window.alert('[ERRO] Digite um Valor!')
-        res.innerHTML = ''
+const button = document.querySelector("#button")
+button.addEventListener("click", () =>{
+    var tabuada = document.querySelector("#tab")
+    var res = document.querySelector("#res")
+    var tab = Number(tabuada.value)
+    if (tabuada.value.length == 0 | tab == 0){
+        window.alert("[ERRO] Digite um Valor!")
+        res.innerHTML = ""  
     }else{
-        res.innerHTML = `Tabuada do ${tab} <hr>`
-        for (let s = 1;s <= 10; s++){
-            res.innerHTML += `${s} x ${tab} = ${s*tab}<br>`
+        res.innerHTML = `<strong>Tabuada do ${tab}</strong> <hr>`
+        for (var number = 1; number <= 10; number ++){
+            res.innerHTML += `${number} x ${tab} = ${number*tab}<br>`
         }
     }
-}
+})
